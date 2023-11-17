197c3b840f5ab174835db998b2020654a6fcdddb
Naftaly <80479583+NaftalyMurimi@users.noreply.github.com>
2023-11-17 15:25:25 +0300

    footer update

--- website/forms.py
+++ website/forms.py
@@ -6,4 +6,5 @@ class MyModelForm(forms.ModelForm):
     class Meta:
         model = ContactModel
         fields = ['username', 'user_email', 'Phone_no' ,'email_subject', 'email_message']
+        
    
